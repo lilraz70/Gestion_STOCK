42d32ad126ff1c994b93e0e7648948b2e1f5ceb4
fayco226 <faicallengane55@gmail.com>
2023-12-08 18:45:49 +0000

    save

--- GESTION_STOCK_PROJET/gestion_stock_app/models.py
+++ GESTION_STOCK_PROJET/gestion_stock_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Categories_Produit(models.Model):
@@ -13,6 +16,9 @@ class Categories_Produit(models.Model):
 
     def __str__(self):
         return self.nom_categorie
+    
+    def update_nombre_produit(self):
+        self.total_produits = Produit.objects.filter(categorie = self ).count()
 
 
 class Produit(models.Model):
@@ -30,4 +36,23 @@ class Produit(models.Model):
 
     def __str__(self):
         return self.nom_produit
+
+@receiver(post_save,sender=Produit)
+def set_nombre_produit(sender, instance, **kwargs):
+    categorie = Categories_Produit.objects.get(pk = instance.categorie.pk) 
+    categorie.update_nombre_produit()
+    
+class Stock(models.Model):
+    produit = models.ForeignKey(Produit, on_delete=models.SET_NULL)
+    total_produit_initial = models.IntegerField()
+    total_produit_final = models.IntegerField()
+    
+
+    class Meta:
+        verbose_name = ("Stock")
+        verbose_name_plural = ("Stocks")
+
+    def __str__(self):
+        return self.name
+
 
