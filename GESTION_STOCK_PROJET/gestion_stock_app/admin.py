01be6db908c8731ef5f3956722164985e681381c
fayco226 <faicallengane55@gmail.com>
2023-12-08 21:58:55 +0000

    save

--- GESTION_STOCK_PROJET/gestion_stock_app/admin.py
+++ GESTION_STOCK_PROJET/gestion_stock_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Fournisseur, Grossiste, Client, Categories_Produit, Produit, Stock, Sortie_grossiste, Sortie_client, Entrer
 
 # Register your models here.
+admin.site.register(Fournisseur)
+admin.site.register(Grossiste)
+admin.site.register(Client)
+admin.site.register(Categories_Produit)
+admin.site.register(Produit)
+admin.site.register(Sortie_client)
+admin.site.register(Sortie_grossiste)
+admin.site.register(Stock)
+admin.site.register(Entrer)
