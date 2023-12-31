2250e4f882cccbe39b80730cd81a06cd1d30de20
Nellehmdlll <77837338+Nellehmdlll@users.noreply.github.com>
2023-12-31 07:48:19 +0000

    hh

--- GESTION_STOCK_PROJET/GESTION_STOCK_PROJET/settings.py
+++ GESTION_STOCK_PROJET/GESTION_STOCK_PROJET/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # location of your application, should not be public web accessible 
-    './static',
+    '../static',
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
